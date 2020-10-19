38dd0cbe59de321e43deb027ab4e5546313c8399
Gabriel de Carvalho <gabriel.de.carvalho_@hotmail.com>
2020-10-19 17:49:37 -0300

    Fix onload function, callbacks in fetch. Add card background color according to it's type.

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -1,24 +1,13 @@
-var pokemonArray =[];
-
-async function fetchData(url) {
+function fetchData(url) {
     return fetch(url)
-      .then(response => {
-        return response.json();
-      })
+      .then(response => response.json())
       .then(data => {
-        console.log(data.results);
-        const pokemons = data.results;
-        pokemons.forEach(pokemon => {
-          fetch(pokemon.url)
-            .then(response => {
-              return response.json();
-            })
-            .then(data => {
-              pokemonArray.push(data);
-            })
-        })
         URL_TO_FETCH = data.next;
-        console.log(URL_TO_FETCH);
-        return data;
-    })
+        const pokemons = data.results;
+        return Promise.all(pokemons.map(pokemon => 
+          fetch(pokemon.url)
+            .then(response => response.json())
+        ))
+      })
+      .then(promiseAllResponse => promiseAllResponse);
 } 
--- src/index.js
+++ src/index.js
@@ -1,64 +1,95 @@
 let calls = 0;
 
-function showPokemons(pokemons) {
-    const pokemonList = pokemons.results.map((pokemon, id) => {
-        return `<li>
-            <a class="card">
+let URL_TO_FETCH = "https://pokeapi.co/api/v2/pokemon?limit=30";
+
+let pokemonArray = [];
+
+function showPokemons(data) {
+    const pokemonList = data.map(pokemon => {
+            return `<li>
+            <a class="card" style="background:${types.find(type => type.name == pokemon.types[0].type.name).card}">
             <div class="pokemon-info">
                 <span class="pokemon-number">
-                #${makeId(id + 1, 3)}
+                #${makeId(pokemon.id, 3)}
                 </span>
                 <span class="pokemon-name">
-                ${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+                ${capitalizeFirstLetter(pokemon.name)}
                 </span>
                 <ul class="pokemon-type">
-                <li class="badge">
-                    <img src="https://raw.githubusercontent.com/vinicoder/pokedex/9a20a03d88361a15b20b450e8933b79e9f175259/src/assets/icons/types/Grass.svg" alt="Grass">
-                    Grass
-                </li>
+                <li class="badge" style="background:${types.find(type => type.name == pokemon.types[0].type.name).color}">
+                    <img src="https://raw.githubusercontent.com/vinicoder/pokedex/9a20a03d88361a15b20b450e8933b79e9f175259/src/assets/icons/types/${capitalizeFirstLetter(pokemon.types[0].type.name)}.svg" 
+                    alt="${pokemon.types[0].type.name}">
+                    ${capitalizeFirstLetter(pokemon.types[0].type.name)}
+                </li>` +
+                (pokemon.types[1] ? `
+                <li class="badge" style="background:${types.find(type => type.name == pokemon.types[1].type.name).color}">
+                    <img src="https://raw.githubusercontent.com/vinicoder/pokedex/9a20a03d88361a15b20b450e8933b79e9f175259/src/assets/icons/types/${capitalizeFirstLetter(pokemon.types[1].type.name)}.svg" 
+                    alt="${pokemon.types[1].type.name}">
+                    ${capitalizeFirstLetter(pokemon.types[1].type.name)}
+                </li>` 
+                : "") +
+                `
                 </ul>
             </div>
-                <img src="https://raw.githubusercontent.com/jnovack/pokemon-svg/3c3ea26da58331d7202e7cdb1aab9b8347d8587f/svg/${(id + 1) + (calls*30)}.svg">
+                <img src="https://raw.githubusercontent.com/jnovack/pokemon-svg/3c3ea26da58331d7202e7cdb1aab9b8347d8587f/svg/${(pokemon.id)}.svg">
             </a>
         </li>
         `;
-        }
-    )
+    })
     .join("");
     console.log(pokemonList);
     document.querySelector("#pokemon-list")
     .insertAdjacentHTML("beforeend", pokemonList);
 }
 
-window.onscroll = function() {
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        loadPokemons();
-    }
-};
-
-function firstLoad() {
-    calls = 0;
-    document.getElementById('loader').style.display = 'none';
-    fetchData("https://pokeapi.co/api/v2/pokemon?limit=30").then(pokemons => {
-        showPokemons(pokemons);
-    })
-        .catch(error => {
-            console.log(error)
-        })
-}
-
 function loadPokemons() {
     calls++;
-    fetchData(URL_TO_FETCH).then(pokemons => {
-        showPokemons(pokemons);
+    fetchData(URL_TO_FETCH).then(data => {
+        pokemonArray = [...pokemonArray, ...data];
+        showPokemons(data)
     })
-        .catch(error => {
-            console.log(error)
-        })
+    .catch(error => {
+        console.log(error)
+    })
+    document.getElementById('loader').style.display = 'none';
 }
+
+window.onscroll = function() {
+    document.getElementById('loader').style.display = 'flex';
+
+    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
+        loadPokemons();
+        document.getElementById('loader').style.display = 'none';
+    }
+};
 
 
 function makeId(id, length) {
     var len = length - (''+ id).length;
     return (len > 0 ? new Array(++len).join('0') : '') + id;
 }  
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const types = [
+    {name: "normal", color: '#A8A77A', card: '#8a8969'},
+    {name: "fire", color: '#EE8130', card: '#f28e49'},
+    {name: "water", color: '#6390F0', card: '#739df5'},
+	{name: "electric", color: '#F7D02C', card: '#e6bc10'},
+	{name: "grass", color: '#7AC74C', card: '#63a163'},
+	{name: "ice", color: '#96D9D6', card: '#75c7c3'},
+	{name: "fighting", color: '#C22E28', card: '#8f4542'},
+	{name: "poison", color: '#A33EA1', card: '#a865a7'},
+	{name: "ground", color: '#E2BF65', card: '#a18b54'},
+	{name: "flying", color: '#A98FF3', card: '#bba6f5'},
+	{name: "psychic", color: '#F95587', card: '#c75b7c'},
+	{name: "bug", color: '#A6B91A', card: '#798714'},
+	{name: "rock", color: '#B6A136', card: '#85762e'},
+	{name: "ghost", color: '#735797', card: '#7a6791'},
+	{name: "dragon", color: '#6F35FC', card: '#4923a6'},
+	{name: "dark", color: '#705746', card: '#665e58'},
+	{name: "steel", color: '#B7B7CE', card: '#8e8ea3'},
+	{name: "fairy", color: '#D685AD', card: '#deb8cb'},
+]
