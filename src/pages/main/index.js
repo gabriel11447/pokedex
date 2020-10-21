99bf0ddb91b09c5383953e666e1617ac6f77593e
Gabriel de Carvalho <gabriel.de.carvalho_@hotmail.com>
2020-10-21 17:07:07 -0300

    Add Roboto font and remove base html from pokemon page (using info from API)

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -4,9 +4,6 @@ let URL_TO_FETCH = "https://pokeapi.co/api/v2/pokemon?limit=30";
 
 let pokemonArray = [];
 
-const badgeUrl = "https://raw.githubusercontent.com/vinicoder/pokedex/9a20a03d88361a15b20b450e8933b79e9f175259/src/assets/icons/types/";
-const spriteUrl = "https://raw.githubusercontent.com/jnovack/pokemon-svg/3c3ea26da58331d7202e7cdb1aab9b8347d8587f/svg/";
-
 function showPokemons(data) {
     const pokemonList = data.map(pokemon => {
             return `<li>
@@ -34,7 +31,7 @@ function showPokemons(data) {
                 `
                 </ul>
             </div>
-                <img src="${spriteUrl + (pokemon.id)}.svg">
+                <img src="${spriteUrl + (pokemon.id)}.png">
             </a>
         </li>
         `;
@@ -66,37 +63,6 @@ window.onscroll = function() {
     }
 };
 
-
-function makeId(id, length) {
-    var len = length - (''+ id).length;
-    return (len > 0 ? new Array(++len).join('0') : '') + id;
-}  
-
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
 function storePokemonArray(id) {
     window.localStorage.setItem('pokemons', JSON.stringify(pokemonArray[id - 1]));
 }
-
-const types = [
-    {name: "normal", color: '#A8A77A', card: '#8a8969'},
-    {name: "fire", color: '#EE8130', card: '#f28e49'},
-    {name: "water", color: '#6390F0', card: '#739df5'},
-	{name: "electric", color: '#F7D02C', card: '#c9a410'},
-	{name: "grass", color: '#7AC74C', card: '#63a163'},
-	{name: "ice", color: '#96D9D6', card: '#75c7c3'},
-	{name: "fighting", color: '#C22E28', card: '#8f4542'},
-	{name: "poison", color: '#A33EA1', card: '#a865a7'},
-	{name: "ground", color: '#E2BF65', card: '#a18b54'},
-	{name: "flying", color: '#A98FF3', card: '#bba6f5'},
-	{name: "psychic", color: '#F95587', card: '#c75b7c'},
-	{name: "bug", color: '#A6B91A', card: '#798714'},
-	{name: "rock", color: '#B6A136', card: '#85762e'},
-	{name: "ghost", color: '#735797', card: '#7a6791'},
-	{name: "dragon", color: '#6F35FC', card: '#4923a6'},
-	{name: "dark", color: '#705746', card: '#665e58'},
-	{name: "steel", color: '#B7B7CE', card: '#8e8ea3'},
-	{name: "fairy", color: '#D685AD', card: '#deb8cb'},
-]
--- src/pages/pokemon/pokemon.js
+++ src/pages/pokemon/pokemon.js
@@ -9,9 +9,98 @@ window.makeActive = function(event) {
 
 function getPokemonArray() {
     pokemonArray = JSON.parse(window.localStorage.getItem('pokemons'));
-    //localStorage.clear();
-    console.log(pokemonArray);
-    const height = pokemonArray.height;
-    document.querySelector("#info")
-    .insertAdjacentHTML("afterbegin", height);
+    //  localStorage.clear();
+    makePokemonCard();
+    makePokemonInfo();
+}
+
+function makePokemonCard() {
+  const pokemonCard = `
+  <div class="card" style="background:${types.find(type => type.name == pokemonArray.types[0].type.name).card}">
+    <img src="${spriteUrl + pokemonArray.id}.png" alt="${pokemonArray.name}">
+    <div class="pokemon-info">
+        <span class="pokemon-number">
+            #${makeId(pokemonArray.id, 3)}
+        </span>
+        <span class="pokemon-name">
+            ${capitalizeFirstLetter(pokemonArray.species.name)}
+        </span>
+        <ul class="pokemon-type">
+          <li class="badge" style="background:${types.find(type => type.name == pokemonArray.types[0].type.name).color}">
+            <img src="${badgeUrl + capitalizeFirstLetter(pokemonArray.types[0].type.name)}.svg" 
+            alt="${pokemonArray.types[0].type.name}">
+            ${capitalizeFirstLetter(pokemonArray.types[0].type.name)}
+          </li>` +
+          (pokemonArray.types[1] ? `
+          <li class="badge" style="background:${types.find(type => type.name == pokemonArray.types[1].type.name).color}">
+            <img src="${badgeUrl + capitalizeFirstLetter(pokemonArray.types[1].type.name)}.svg" 
+            alt="${pokemonArray.types[1].type.name}">
+            ${capitalizeFirstLetter(pokemonArray.types[1].type.name)}
+          </li>` 
+    : "") +
+    `
+        </ul>
+    </div>
+  </div>
+  `;
+  document.querySelector("header")
+  .insertAdjacentHTML("afterbegin", pokemonCard);
+}
+
+function makePokemonInfo() {
+  const pokemonInfo = `
+  <h2>Pokédex Data</h2>
+  <div class="pokemon-data">
+      <ul>
+          <li>
+              <span>
+                  <b>Species</b>
+                  ${pokemonArray.species.name}
+              </span>
+          </li>
+          <li>
+              <span>
+                  <b>Height</b>
+                  0.${pokemonArray.height}m
+              </span>
+          </li>
+          <li>
+              <span>
+                  <b>Weight</b>
+                  ${pokemonArray.weight}kg
+              </span>
+          </li>
+          <li>
+              <span>
+                  <b>Abilites</b>
+                  1. ${capitalizeFirstLetter(pokemonArray.abilities[0].ability.name)}
+                  2. ${capitalizeFirstLetter(pokemonArray.abilities[1].ability.name)}
+              </span>
+          </li>
+      </ul>
+  </div>            
+  `;
+  document.querySelector("#info")
+  .insertAdjacentHTML("afterbegin", pokemonInfo);
+
+  const pokemonData = `
+  <h2>Stats</h2>
+    <div class="pokemon-stats">
+        <ul>` +
+        (pokemonArray.stats.map(status => {
+            return `
+            <li>
+                <span>
+                    <b>${capitalizeFirstLetter(status.stat.name)}</b>
+                    ${status.base_stat}
+                </span>
+            </li>
+            `
+        })
+        .join("")) + `
+        </ul>
+    </div>
+  `;
+  document.querySelector("#stats")
+  .insertAdjacentHTML("afterbegin", pokemonData);
 }
