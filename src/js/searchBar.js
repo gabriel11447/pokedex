8916477ed26f9ebbea117f42674df6a8d9b33c70
Gabriel de Carvalho <gabriel.de.carvalho_@hotmail.com>
2020-10-27 19:16:20 -0300

    Add search by id

--- src/js/searchBar.js
+++ src/js/searchBar.js
@@ -1,7 +1,6 @@
 function removeSearchedPokemon() {
     document.getElementById("pokemon-list").remove();
     let size = searchedPokemons.length - 1;
-    console.log(searchedPokemons);
     for (let i = 0; i < size; i++) {
         searchedPokemons.pop();
     }
@@ -10,29 +9,37 @@ function removeSearchedPokemon() {
 const inputSearch = document.getElementById("search");
 inputSearch.addEventListener("keyup", (event) => {
     if (event.key === 'Enter') {
+        document.getElementById('loader').classList.remove("hidden");
+        document.getElementById("error").classList.add('hidden');
         removeSearchedPokemon();
         if (inputSearch.value != "") {
-            flagSearch = 1;
-            document.getElementById('loader').classList.remove("hidden");
-            searchedPokemons = pokemonArray.filter(input => input.name.includes(inputSearch.value.toLowerCase()));
+            let inputNumber = parseInt(inputSearch.value);
+            if (!isNaN(inputNumber)) {
+                searchedPokemons = pokemonArray.filter((input, index) => {
+                    if ((index + 1).toString().includes(inputSearch.value)) {
+                        return input;
+                    }
+            })
+            } else {
+                searchedPokemons = pokemonArray.filter(input => input.name.includes(inputSearch.value.toLowerCase()));
+            }
             document.getElementById("card-list")
             .insertAdjacentHTML("afterbegin", `<ul id="pokemon-list" class="pokemon-list"></ul>`);
             if (searchedPokemons.length > 0) {
-                document.getElementById("error").classList.add('hidden');
                 showSearchedPokemons(searchedPokemons);
             } else {
-                flagSearch = 0;
                 const error = document.getElementById('error');
                 error.innerHTML = "<p>0 results found.</p>"
+                document.getElementById("loader").classList.add('hidden');
                 error.classList.remove('hidden');
             }
-            document.getElementById('loader').classList.add("hidden");
             inputSearch.value = "";
             inputSearch.blur()
         } else {
             document.getElementById("card-list")
             .insertAdjacentHTML("afterbegin", `<ul id="pokemon-list" class="pokemon-list"></ul>`);
             calls = 0;
+            window.addEventListener('scroll', infiniteScroll);
             renderPokemons();
         }
     }
