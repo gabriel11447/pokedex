0ec34af95a88c7baf264e3c39ef33590437148ec
Gabriel de Carvalho <gabriel.de.carvalho_@hotmail.com>
2020-11-10 07:54:40 -0300

    Fix infinite scroll

--- src/js/infiniteScroll.js
+++ src/js/infiniteScroll.js
@@ -1,8 +1,8 @@
-function infiniteScroll() {
+async function infiniteScroll() {
     document.getElementById('loader').classList.remove("hidden");
 
-    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
-        renderPokemons();
+    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 1) {
+        await renderPokemons();
         document.getElementById('loader').classList.add("hidden");
     }
 }
